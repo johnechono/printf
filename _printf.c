14075f5fd1eb6de282f0b36fa519e27db1c9d10e
job kumdan <redeks123456@gmail.com>
2023-03-27 15:05:37 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -1,11 +1,16 @@
 #include "main.h"
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
